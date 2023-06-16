08c14dbeacdd2ad6062ca35d39f6c5527ff0340c
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-06-16 17:05:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+switchToSignUp = () => {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("signup").style.display = "block";
+}
+switchToLogin = () => {
+    document.getElementById("login").style.display = "block";
+    document.getElementById("signup").style.display = "none";
+}
