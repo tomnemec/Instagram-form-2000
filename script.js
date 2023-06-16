0ff03ecff0547c722ff6839bb5193e989dcda19e
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-06-16 18:20:44 +0200

    fadeout animation

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 switchToSignUp = () => {
+    const card = document.getElementById("signup");
+    card.style.animationName = "fadeOut";
+    card.style.animationDuration = "1s";
     document.getElementById("login").style.display = "none";
     document.getElementById("signup").style.display = "block";
 }
 switchToLogin = () => {
+    const card = document.getElementById("login");
+    card.style.animationName = "fadeOut";
+    card.style.animationDuration = "1s";
     document.getElementById("login").style.display = "block";
     document.getElementById("signup").style.display = "none";
 }
